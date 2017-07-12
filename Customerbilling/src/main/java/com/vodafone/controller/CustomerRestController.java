7155e4148028251b4a1a99c78a8611ce14415ecc
LAPTOP-39F3KLHA\bothinah <bothinah.mostafa1@gmail.com>
2017-07-12 11:53:31 +0200

    no message

--- Customerbilling/src/main/java/com/vodafone/controller/CustomerRestController.java
+++ Customerbilling/src/main/java/com/vodafone/controller/CustomerRestController.java
@@ -1,5 +1,34 @@
 package com.vodafone.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vodafone.dao.CustomerDAO;
+import com.vodafone.model.Customer;
+
+@RequestMapping("/Customers")
+@RestController
+@Secured("ROLE_ADMIN")
+
 public class CustomerRestController {
+	CustomerDAO customerDAO;
+	
+	@Autowired
+	public CustomerRestController(CustomerDAO customerDAO){
+		this.customerDAO = customerDAO;
+	}
+	
+	@RequestMapping(method=RequestMethod.GET , produces="application/json")
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	public List<Customer> getAllCustomers(){
+		System.out.println("Inside getAllCustomers.");
+		return customerDAO.findAll();
+	}
 
 }
