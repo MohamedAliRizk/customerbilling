ff93fd16d4ce65f54e4c096e000c3428cbda8d80
AmrKhalafSaber <amr.kh.saber@gmail.com>
2017-07-27 13:30:34 +0300

    Apply validation in project

--- Customerbilling/src/main/java/com/vodafone/model/FullName.java
+++ Customerbilling/src/main/java/com/vodafone/model/FullName.java
@@ -1,12 +1,23 @@
 package com.vodafone.model;
 
+
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import com.vodafone.groups.Update;
+
 import lombok.Data;
 import lombok.ToString;
 
 @ToString(includeFieldNames=true)
 @Data
 public class FullName {
-	private String firstName, middleName, lastName;
+	@NotEmpty(groups={Update.class},message="first Name is mandatory") 
+	private  String  firstName;
+	
+	private  String middleName; 
+	@NotEmpty(groups={Update.class},message="last name is mandatory ")
+	private  String lastName;
 
 	
 	public FullName(String firstName, String middleName, String lastName) {
